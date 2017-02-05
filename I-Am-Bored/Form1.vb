2b5fa730b1866eeb6af9c7d86e566572b8fe8529
SlipSerum <appsbygamer@yahoo.co.uk>
2017-02-05 11:52:58 -0800

    1.0b - Source
    This is the source to v1.0b

---
+++ I-Am-Bored/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        System.Diagnostics.Process.Start("http://patreon.com/SlipSerum")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Static hits As Integer = 0
+        hits += 1
+
+        Label1.Text = hits.ToString("n0")
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
